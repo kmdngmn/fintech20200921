b89db0777116215ace890c9729a7f18bcd07e309
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 16:06:12 +0900

    Request 완성

--- http_server.js
+++ http_server.js
@@ -1,11 +1,15 @@
 var http = require("http");
+const request = require("request");
+var parseString = require("xml2js").parseString;
 
 http
   .createServer(function (req, res) {
     var body = "<html><h1>test</h1></html>";
     console.log("요청이 들어왔습니다.");
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.end(body);
+    request("https://naver.com", function (error, response, body) {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.end(body);
+    });
   })
   .listen(3000);
 
