0727a590adcdefcce89e30e448cb38193987f52c
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 17:02:17 +0900

    데이터베이스 user 테이블에 있는 목록 전체 조회하기

--- mysql_test.js
+++ mysql_test.js
@@ -2,15 +2,14 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1q2w3e4r",
+  password: "1q2w3e4r", //본인 비밀번호
   database: "fintech090921",
 });
 
 connection.connect();
-
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+connection.query("SELECT * FROM user", function (error, results, fields) {
   if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
+  console.log(results);
 });
 
 connection.end();
