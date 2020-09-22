21de540305764ba2fe6558a956f52b97e89d7f4d
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 15:44:18 +0900

    #work5 xml 파싱후 데이터 선정

--- request.js
+++ request.js
@@ -1,8 +1,13 @@
 const request = require("request");
+var parseString = require("xml2js").parseString;
+
 request(
   "https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109",
   function (error, response, body) {
-    console.log("body:", body);
-    console.log(body.rss.channel.title);
+    var xml = body;
+    parseString(xml, function (err, result) {
+      console.dir(result.rss.channel);
+      //#work5 기상 예보 내역을 출력하기
+    });
   }
 );
