2c89eb42f78bace1392ddc8ba1700e7ebda88895
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 15:30:05 +0900

    multer 삭제

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
 const request = require("request");
-request("http://www.google.com", function (error, response, body) {
+request("http://www.naver.com", function (error, response, body) {
   console.error("error:", error); // Print the error if one occurred
   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   console.log("body:", body); // Print the HTML for the Google homepage.
