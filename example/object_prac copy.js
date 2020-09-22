cd4308aea6b5de65b309fc40fafc5dc3b309812e
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 11:30:51 +0900

    최종확인 샘플코드

---
+++ example/object_prac copy.js
@@ -0,0 +1,34 @@
+var car = {
+  name: "sonata",
+  ph: "500ph",
+  start: function () {
+    console.log("engine is starting");
+  },
+  stop: function () {
+    console.log("engine is stoped");
+  },
+};
+
+var car2 = {
+  name: "bmw",
+  ph: "380ph",
+  start: function () {
+    console.log("engine is starting");
+  },
+  stop: function () {
+    console.log("engine is stoped");
+  },
+};
+
+var car3 = {
+  name: "smart",
+  ph: "100ph",
+  start: function () {
+    console.log("engine is starting");
+  },
+  stop: function () {
+    console.log("engine is stoped");
+  },
+};
+
+var cars = [car, car2, car3];
