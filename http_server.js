ba91a3f96eae4a3bbd389881899915f91cfed1d8
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 16:43:00 +0900

    npm i mysql mysql 연결 모듈 추가

--- http_server.js
+++ http_server.js
@@ -4,7 +4,6 @@ var parseString = require("xml2js").parseString;
 
 http
   .createServer(function (req, res) {
-    var body = "<html><h1>test</h1></html>";
     console.log("요청이 들어왔습니다.");
     request("https://naver.com", function (error, response, body) {
       res.setHeader("Content-Type", "text/html; charset=utf-8");
