096899280ce4d32f47edb207894141db240e5ccd
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 15:33:41 +0900

    naver => 날씨 api 변경

--- request.js
+++ request.js
@@ -1,6 +1,7 @@
 const request = require("request");
-request("http://www.naver.com", function (error, response, body) {
-  console.error("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
-});
+request(
+  "https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109",
+  function (error, response, body) {
+    console.log("body:", body);
+  }
+);
