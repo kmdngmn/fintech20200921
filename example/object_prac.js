e62e417aab9d2fda54e16cd48ba6e7fb14d55931
gwanwoo-you <skymill2000@gmail.com>
2020-09-22 10:50:47 +0900

    object 연습

---
+++ example/object_prac.js
@@ -0,0 +1,23 @@
+var car = {
+  name: "sonata",
+  ph: "500ph",
+  start: function () {
+    console.log("engine is starting");
+  },
+  stop: function () {
+    console.log("engine is stoped");
+  },
+};
+
+var car2 = {
+  name: "bmw",
+  ph: "380ph",
+  start: function () {
+    console.log("engine is starting");
+  },
+  stop: function () {
+    console.log("engine is stoped");
+  },
+};
+
+console.log(car);
