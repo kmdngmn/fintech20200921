cbd4476bffe88d1cfbe5113376830940cb8752dd
gwanwoo-you <skymill2000@gmail.com>
2020-09-23 10:28:42 +0900

    데이터 입력 html 추가

--- express_server.js
+++ express_server.js
@@ -8,13 +8,8 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.get("/hello", function (req, res) {
-  res.send("Hello 2");
-});
-
-//#work6 다양한 라우터를 추가해보세요
-app.get("/user", function (req, res) {
-  res.send("user Data");
+app.get("/ejsTest", function (req, res) {
+  res.render("test");
 });
 
 app.listen(3000);
