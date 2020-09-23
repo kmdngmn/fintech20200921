6544d2382640146454236beb2fa07de79a8d6dba
gwanwoo-you <skymill2000@gmail.com>
2020-09-23 11:06:08 +0900

    사용자 데이터를 받기위한 EXPRESS 옵션 설정

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const app = express();
 app.set("views", __dirname + "/views"); //ejs 를 사용하기위한 디렉토리 설정
 app.set("view engine", "ejs"); //ejs 를 사용하기위한 뷰 엔진 설정
 
+app.use(express.json()); // JSON 타입의 데이터를 받기위한 설정
+app.use(express.urlencoded({ extended: false })); // urlencoded 타입의 데이터를 받기위한 설정
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -15,6 +18,7 @@ app.get("/ejsTest", function (req, res) {
 app.post("/getDataTest", function (req, res) {
   var userText = req.body.userText;
   console.log(userText);
+  res.json("입력값은 : " + userText);
 });
 
 app.listen(3000);
