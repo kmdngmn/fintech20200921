582f663fb8049972d3708841af71752574b3719e
gwanwoo-you <skymill2000@gmail.com>
2020-09-24 16:50:33 +0900

    qr 코드 렌더링 페이지 추가

--- express_server.js
+++ express_server.js
@@ -45,6 +45,10 @@ app.get("/balance", function (req, res) {
   res.render("balance");
 });
 
+app.get("/qrcode", function (req, res) {
+  res.render("qrcode");
+});
+
 // app.get("/authText", auth, function (req, res) {
 //   res.json("당신은 콘텐츠 접근에 성공했습니다.");
 // });
